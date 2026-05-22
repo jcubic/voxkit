dade1661934a7b4acd6418df937586a2949393a4
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-05-22 14:00:09 +0200

    improve license comment

--- src/voxkit/tts.py
+++ src/voxkit/tts.py
@@ -1,5 +1,7 @@
 # Copyright (C) 2026 Jakub T. Jankiewicz <https://jakub.jankiewicz.org/>
 #
+# This file is part of VoxKit.
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
